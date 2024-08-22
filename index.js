7ad225e903601cf01e32580de27e719ddd160e6e
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-08-22 14:09:40 -0700

    Completed event listener lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    // Select the button element by its id
+    const input = document.getElementById('button');
+  
+    // Define the function that will be called when the button is clicked
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add an event listener to the button element
+    input.addEventListener('click', clickAlert);
+  }
+  
